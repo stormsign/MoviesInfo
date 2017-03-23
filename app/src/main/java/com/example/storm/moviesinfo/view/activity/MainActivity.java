49bef9f3ea0a19897c9fe68c848719ff2dfc5a93
stormsign <1009784927@qq.com>
2017-03-24 01:06:43 +0800

    initial commit

--- app/src/main/java/com/example/storm/moviesinfo/view/activity/MainActivity.java
+++ app/src/main/java/com/example/storm/moviesinfo/view/activity/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.storm.moviesinfo.view.activity;
 import android.os.Bundle;
 import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -11,7 +12,9 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
 import com.example.storm.moviesinfo.R;
 
 import butterknife.BindView;
@@ -25,6 +28,8 @@ public class MainActivity extends AppCompatActivity  {
     AppBarLayout mAppBar;
     @BindView(R.id.main_toolbar)
     Toolbar mToolBar;
+    @BindView(R.id.main_nav)
+    NavigationView mNav;
 
     private boolean isAppBarCollapsed;
 
@@ -49,10 +54,16 @@ public class MainActivity extends AppCompatActivity  {
                         .setAction("Action", null).show();
             }
         });
+
+        //toolbar左侧的material design风格箭头
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, mDrawer, mToolBar,
                 R.string.openDrawerContentDesc, R.string.closeDrawerContentDesc);
         mDrawer.addDrawerListener(toggle);
         toggle.syncState();
+
+        //左侧抽屉导航栏
+        ImageView drawerHeaderImg = (ImageView) mNav.getHeaderView(0).findViewById(R.id.drawer_header_img);
+        Glide.with(this).load(R.drawable.pic_movies);
     }
 
     @Override
